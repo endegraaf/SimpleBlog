aefd083f735adea95b919ad89b78f7ddfa4271e7
eric <eric@eric-Latitude-E7440>
2016-04-11 10:17:30 +0200

    kljdglkdxflgkx

--- src/main/java/com/rohan/blog/authentication/businessservice/AuthenticationModelBS.java
+++ src/main/java/com/rohan/blog/authentication/businessservice/AuthenticationModelBS.java
@@ -5,7 +5,7 @@ import org.springframework.security.access.annotation.Secured;
 public interface AuthenticationModelBS {
 	
 	boolean login(String username, String password);
-
+ 
 	@Secured({"ROLE_ADMIN","ROLE_REGISTERED", "ROLE_USER"})
 	void logout();
 }
