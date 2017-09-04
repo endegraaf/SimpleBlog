cd7660c6f8b12eee7c0cb1cef4db3f69bb080845
Eric de Graaf <endegraaf@gmail.com>
2017-09-04 14:28:22 +0200

    update steps and feature

--- src/test/java/com/rohan/blog/steps/BlogSteps.java
+++ src/test/java/com/rohan/blog/steps/BlogSteps.java
@@ -7,15 +7,20 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.After;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 /**
  * http://blog.czeczotka.com/2014/07/22/cucumber-jvm-with-maven-in-minutes/
  */
 public class BlogSteps {
 
-    public static final String BLOG_URL = "http://localhost:8088/blog";
+    public static final String BLOG_URL = "http://altenit-ubuntu01.cloudapp.net:8088/blog";
     public WebDriver driver;
 
 
@@ -37,9 +42,66 @@ public class BlogSteps {
 
     @Then("^I see the latest blog posts$")
     public void i_see_the_latest_blog_posts() throws Throwable {
+        List<WebElement> rows = driver.findElements(By.cssSelector(".ui-datagrid-column1"));
+        Assert.assertNotEquals("Check that there are blog post rows", rows.size(), 0);
+    }
+
+
+    @Given("^I am a blog editor$")
+    public void i_am_a_blog_editor() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
+
+    @When("^I log into the blog using a username and password$")
+    public void i_log_into_the_blog_using_a_username_and_password() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should be logged and see additional functionality only visible to editors$")
+    public void i_should_be_logged_and_see_additional_functionality_only_visible_to_editors() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I log into the blog$")
+    public void i_log_into_the_blog() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^there are existing blog posts$")
+    public void there_are_existing_blog_posts() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should be able to Edit the blog content$")
+    public void i_should_be_able_to_Edit_the_blog_content() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I log in to the blog$")
+    public void i_log_in_to_the_blog() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should be able to Create a new post$")
+    public void i_should_be_able_to_Create_a_new_post() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should be able to Delete a new post$")
+    public void i_should_be_able_to_Delete_a_new_post() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
 
     @After
     public void tearDown() {
--- src/test/resources/cucumber/blog.feature
+++ src/test/resources/cucumber/blog.feature
@@ -3,8 +3,25 @@ Feature: Blog
   I want to see blog posts
   So that I can see what's going on
 
-  Scenario:
+  @primary
+  Scenario: blog visitor
     Given I am a blog visitor
     When I open the blog
     Then I see the latest blog posts
-
+#
+#  Scenario:
+#    Given I am a blog editor
+#    When I log into the blog using a username and password
+#    Then I should be logged and see additional functionality only visible to editors
+#
+#  Scenario:
+#    Given I am a blog editor
+#    When I log into the blog
+#    And there are existing blog posts
+#    Then I should be able to Edit the blog content
+#
+#  Scenario:
+#    Given I am a blog editor
+#    When I log in to the blog
+#    Then I should be able to Create a new post
+#    And I should be able to Delete a new post
